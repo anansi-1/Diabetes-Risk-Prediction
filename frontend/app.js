ef284ce9424784879c76055ace4444eefcf18fc2
anansi-1 <anansisime7@gmail.com>
2026-01-02 14:56:40 +0300

    update fetch url

--- frontend/app.js
+++ frontend/app.js
@@ -39,11 +39,14 @@ form.addEventListener("submit", async (e) => {
   }
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
+    const response = await fetch(
+      "https://diabetes-risk-prediction-qtad.onrender.com/predict",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    );
 
     if (!response.ok) {
       const err = await response.json();
